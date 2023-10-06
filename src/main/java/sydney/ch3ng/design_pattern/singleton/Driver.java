7756a2fef06260256a928a5492ec7fde0f390db5
Edward Cheng <edward@cheng.sydney>
2023-10-06 21:08:29 +1100

    refactor

--- src/main/java/sydney/ch3ng/design_pattern/singleton/Driver.java
+++ src/main/java/sydney/ch3ng/design_pattern/singleton/Driver.java
@@ -1,5 +1,8 @@
 package sydney.ch3ng.design_pattern.singleton;
 
+/**
+ * The singleton design pattern is a creational design pattern that ensures that a class has only one instance while providing a global access point to this instance. The singleton pattern is often used when a single object is needed to coordinate actions across the system.
+ */
 public class Driver {
     public static void main(String[] args) {
         Singleton singleton = Singleton.getInstance();
